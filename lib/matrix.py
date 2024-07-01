Fix: overflow in invariant factors

lib/matrix.py
@@ -1,4 +1,3 @@
-import time
 import numpy as np
 
 
@@ -16,72 +15,82 @@ def gcdext(a, b):
         return (d, y, x - y * (a // b))
 
 
+def swap_rows(A, i, j):
+    A[[i, j]] = A[[j, i]]
+
+
+def swap_cols(A, i, j):
+    A[:, [i, j]] = A[:, [j, i]]
+
+
+def add_row(A, i, j, c1: tuple, c2: tuple):
+    temp = c1[0]*A[i, :] + c1[1]*A[j, :]
+    A[j, :] = c2[0]*A[i, :] + c2[1]*A[j, :]
+    A[i, :] = temp
+
+
+def add_col(A, i, j, c1: tuple, c2: tuple):
+    temp = c1[0]*A[:, i] + c1[1]*A[:, j]
+    A[:, j] = c2[0]*A[:, i] + c2[1]*A[:, j]
+    A[:, i] = temp
+
+
+def clear_row(A, t,n):
+    pivot = A[t, t]
+    if pivot == 0:
+        return
+    for j in range(t+1, n):
+        if A[t, j] == 0:
+            continue
+        d, r = divmod(A[t, j], pivot)
+        if r == 0:
+            add_col(A, t, j, (1, 0), (-d, 1))
+        else:
+            g, a, b = gcdext(pivot, A[t, j])
+            d_0 = A[t, j]//g
+            d_j = pivot//g
+            add_col(A, t, j, (a, b), (d_0, -d_j))
+            pivot = g
+
+
+def clear_col(A, t,m):
+    pivot = A[t, t]
+    if pivot == 0:
+        return
+    for i in range(t+1, m):
+        if A[i, t] == 0:
+            continue
+        d, r = divmod(A[i, t], pivot)
+        if r == 0:
+            add_row(A, t, i, (1, 0), (-d, 1))
+        else:
+            g, a, b = gcdext(pivot, A[i, t])
+            d_0 = A[i, t]//g
+            d_i = pivot//g
+            add_row(A, t, i, (a, b), (d_0, -d_i))
+            pivot = g
+
+
 def invariant_factors(mt: np.ndarray):
 
-    def swap_rows(A, i, j):
-        A[[i, j]] = A[[j, i]]
-
-    def swap_cols(A, i, j):
-        A[:, [i, j]] = A[:, [j, i]]
-
-    def add_row(A, i, j, c1: tuple, c2: tuple):
-        temp = c1[0]*A[i, :] + c1[1]*A[j, :]
-        A[j, :] = c2[0]*A[i, :] + c2[1]*A[j, :]
-        A[i, :] = temp
-
-    def add_col(A, i, j, c1: tuple, c2: tuple):
-        temp = c1[0]*A[:, i] + c1[1]*A[:, j]
-        A[:, j] = c2[0]*A[:, i] + c2[1]*A[:, j]
-        A[:, i] = temp
-
-    def clear_row(A, t):
-        pivot = A[t, t]
-        for j in range(t+1, n):
-            if A[t, j] == 0:
-                continue
-            d, r = divmod(A[t, j], pivot)
-            if r == 0:
-                add_col(A, t, j, (1, 0), (-d, 1))
-            else:
-                g, a, b = gcdext(pivot, A[t, j])
-                d_0 = A[t, j]//g
-                d_j = pivot//g
-                add_col(A, t, j, (a, b), (d_0, -d_j))
-                pivot = g
-
-    def clear_col(A, t):
-        pivot = A[t, t]
-        for i in range(t+1, m):
-            if A[i, t] == 0:
-                continue
-            d, r = divmod(A[i, t], pivot)
-            if r == 0:
-                add_row(A, t, i, (1, 0), (-d, 1))
-            else:
-                g, a, b = gcdext(pivot, A[i, t])
-                d_0 = A[i, t]//g
-                d_i = pivot//g
-                add_row(A, t, i, (a, b), (d_0, -d_i))
-                pivot = g
-
-    A = np.matrix(mt)
+    A = np.matrix(mt).astype(object)
     m, n = A.shape
-    # If the first element is zero, swap rows and columns to make it non-zero
     for t in range(min(m, n)):
+
+        # If the first element is zero, swap rows and columns to make it non-zero
         if A[t, t] == 0:
-            non_zero = np.nonzero(A)
+            non_zero = np.nonzero(A[:t, t:])
+            non_zero = (non_zero[0] + t, non_zero[1] + t)
             if non_zero[0].size == 0:
-                return []
+                break
             swap_rows(A, t, non_zero[0][0])
             swap_cols(A, t, non_zero[1][0])
+
         # Clear the first row and column
         while (any(A[t, i] != 0 for i in range(t+1, n)) or
                any(A[i, t] != 0 for i in range(t+1, m))):
-            clear_col(A, t)
-            clear_row(A, t)
-    
-    inv =[abs(int(A[i,i])) for i in range(min(m, n))]
-
-    inv.sort()  
+            clear_col(A, t, m)
+            clear_row(A, t, n)
+    inv = [abs(int(A[i, i])) for i in range(min(m, n))]  # if A[i,i] != 0]
+    inv.sort()
     return inv
-    

test.py
@@ -1,6 +1,7 @@
 from lib.graph import Graph
 from lib.matrix import *
-
+from lib.cube import *
+import cProfile
 # g = Graph(3)
 
 # g.addEdge(1, 0)
@@ -10,13 +11,36 @@ from lib.matrix import *
 # print(g.adjMatrix())
 # print(g.laplacianMatrix())
 
-
-mt = np.array([[2,4,4],
-               [-6,6,12],
-               [10,4,16]])
-mt = np.array([[2,4,6],[1,3,5]])
-mbt = np.array([[4,6]])
-
-inv = invariant_factors(mt)
-
-print(inv)
+np.set_printoptions(linewidth=400)
+mt = np.array([
+    [2, -1, 0, -1, 0, 0],
+    [-1, 4, -1, -1, -1, 0],
+    [0, -1, 2, 0, -1, 0],
+    [-1, -1, 0, 4, -1, -1],
+    [0, -1, -1, -1, 4, -1],
+    [0, 0, 0, -1, -1, 2]
+], dtype=int)
+mt = np.array([
+    [3, -1, -1, -1],
+    [-1, 2, 0, -1],
+    [-1, 0, 2, -1]
+], dtype=int)
+# mt = np.array([
+#     [3, -1, -1, -1],
+#     [-1, 2, 0, -1],
+#     [-1, 0, 2, -1],
+#     [-1, -1, -1, 3]
+# ])
+# mt = np.array([[12, 6, 4], [3, 9, 6], [2, 16, 14]], dtype=int)
+# print(invariant_factors(mt))
+# ncubeRL = ncube_laplacian(4)
+# s = 2**4
+# print(ncubeRL)
+# print(ncubeRL[:s-1, :s-1])
+# print(invariant_factors(ncubeRL))
+# print(invariant_factors(ncubeRL[:s-1, :s-1]))
+def test():
+    for i in range(2, 7):
+        print(i, end=': ')
+        print(invariant_factors(ncube_laplacian(i)[1:, 1:]))
+test()