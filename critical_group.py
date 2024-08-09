be79bbffbd45f8197319dd0752b6b2f37772d237
justin <justin.chen@thinktrace.com>
2024-08-09 13:40:02 +0800

    .

--- critical_group.py
+++ critical_group.py
@@ -5,66 +5,18 @@ from lib.divisor import Divisor
 import numpy as np
 from lib.algorithms import *
 
-# for i in range(2,15):
-#     # l = ncube_laplacian(i)
-#     # print(i, end=': ')
-#     # print(invariant_factors(l[1:, 1:]))
-#     print(f"{i:<3}",get_largest_cyclic_factor_cube(i))
+for n in range(3,7):
+    g = ncube_graph(n)
+    l = g.laplacianMatrix()
+    print(n, end=': ')
+    # print(invariant_factors(l[1:, 1:]))
+    print(np.linalg.det(l[1:, 1:]))
+    print("===")
 
-
-
-# print(prime_factor(129))
-
-
-
-
-
-# for i in range(2, 12):
-#     print(i, end=': ')
-#     print(get_largest_cyclic_factor_cube(i))
-for i in range(2, 12):
-    print(i)
-    n = i
-    N = get_largest_cyclic_factor_cube(n)
+for n in range(3,7):
     g = ncube_reduce_graph(n)
-    d = Divisor(g, [N, -N] + [0]*(2*n-2))
-    a = greedy(d)
-    with  open("critical_group.txt", "a") as f :
-        f.write(f"{i}".center(20, "=")+'\n')
-        if all(i >=0 for i in a):
-            f.write(str(a)+'\n')
-        else:
-            a = [i - min(a) for i in a]
-            f.write(str(a)+'\n')
-
-# n = 10
-# g = ncube_graph(n)
-# lap = g.laplacianMatrix()
-# rscript = [128898, 0, 71540, 57358, 66143, 62755, 65085, 63813, 64768, 64130, 64642, 64256, 64581, 64317, 64547, 64351, 64526, 64372, 64512, 64386]
-# script = [0 for i in range(g.nodenum)]
-
-# for i in range(g.nodenum):
-#     first_bit = 1 if i & (1 << (n-1)) else 0 
-#     k = i & ((1 << (n-1)) - 1)
-
-#     #count how many 1 in k
-#     count = 0
-#     for j in range(n):
-#         if k & (1 << j):
-#             count += 1
-
-#     idx = 2*count + first_bit
-#     # print(i,idx, first_bit, count,bin(k).replace('0b',''))
-#     script[i] = rscript[idx]
-
-# # exit()
-# script = np.array(script).transpose()
-# ans = lap @ script
-# print(get_largest_cyclic_factor_cube(n))
-# print(ans.min())
-# print(ans.max())
-# print(np.count_nonzero(ans))
-# print(ans)
-
-# for n in range(2,14):
-#     print(get_largest_cyclic_factor_cube(n))
+    l = g.laplacianMatrix()
+    print(n, end=': ')
+    # print(np.linalg.det(np.delete(np.delete(l, 1, 0), 1, 1)))
+    print(np.linalg.det(l[1:, 1:]))
+    print("===")
--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from lib.divisor import Divisor
 import numpy as np
 from lib.prime import *
 import math
+import decimal
 
 
 def factorial(n: int) -> int:
@@ -68,16 +69,64 @@ for n, script in data.items():
     # print('------------')
 
 
-for n, script in data.items():
-    print(n, end=': \n')
-    print(script[2*n-2])
-    for p, m in prime_factor(script[2*n-2]):
-        c = FCOLOR.GREEN
-        if p == 2:
-            c = FCOLOR.RED
-        print(f"{color(p,c)}^{m}", end=', ')
-    print()
-    print("-----------")
+# for n, script in data.items():
+#     print(n, end=': \n')
+#     print(script[2*n-2])
+#     for p, m in prime_factor(script[2*n-2]):
+#         c = FCOLOR.GREEN
+#         if p == 2:
+#             c = FCOLOR.RED
+#         print(f"{color(p,c)}^{m}", end=', ')
+#     print()
+#     print("-----------")
+
+# for n,script in data.items():
+#     print(n, end=': \n')
+#     print(get_largest_cyclic_factor_cube(n))
+#     print(script[2*n-1] + (n-1)*script[2*n-4])
+
+# for n,script in data.items():
+#     print(n, end=': \n')
+#     N = get_largest_cyclic_factor_cube(n)
+
+#     print(script[0])
+#     print((1+1/n)*N - (n-1) * script[2*n-4])
+#     print((2)*(N + (N//n))//(n+1))
+
+#     print("-----------")
+
+# for n, script in data.items():
+#     if n == 2:
+#         continue
+#     print(n, end=': \n')
+
+#     pm1 = prime_factor(data[n-1][0])
+#     pm2 = prime_factor(script[2*n-1])
+#     for p in PRIME:
+#         if any(t == p for t, m in pm1) or any(t == p for t, m in pm2):
+#             if any(t == p for t,m in pm1) : 
+#                 m = next(filter(lambda x: x[0] == p, pm1))[1]
+#                 print(f"{p:>3}^{m}", end='  ')
+#             else: 
+#                 print(f"{' ':>5}", end='  ')
+#             if any(t == p for t,m in pm2) :
+#                 m = next(filter(lambda x: x[0] == p, pm2))[1]
+#                 print(f"{p:>3}^{m}", end='  ')
+#             print()
+#     print()
+#     print("-----------")
+
+# for n, script in data.items():
+#     if n== 2:
+#         continue
+#     print(n, end=': \n')
+#     c = data[n-1][0] * (n-1) / script[2*n-1]
+#     print(int(c))
+#     for p,m in (prime_factor(int(c))):
+#         print(f"{p}^{m}", end=', ')
+#     print()
+#     print("-----------")
+
 # for n,script in data.items():
 #     print(f"{n}")
 #     script.remove(0)
@@ -96,10 +145,10 @@ for n, script in data.items():
 #     for i in range(1,len(script),2):
 #         print(f"{script[i]:<{maxnumberlength}}",end='  ')
 #     print()
-    # for i in range(0,len(script)-1,2):
-    #     print(f"\x1b[95m{script[i]-script[i+1]:<{maxnumberlength}}\x1b[0m",end='  ')
-    # print()
-    # print("-----------")
+#     for i in range(0,len(script)-1,2):
+#         print(f"\x1b[95m{script[i]-script[i+1]:<{maxnumberlength}}\x1b[0m",end='  ')
+#     print()
+#     print("-----------")
 
 
 # for n in range(2,20):
@@ -150,3 +199,68 @@ for n, script in data.items():
 #     for f,m in dcompy:
 #         print(f"{f}^{m}", end=', ')
 #     print("\n-----------------")
+
+
+
+for n,script in data.items():
+    if n> 6:
+        break
+    g = ncube_reduce_graph(n)
+    l = g.laplacianMatrix()
+    rl = np.delete(np.delete(l, 1, 0), 1, 1)
+    N = get_largest_cyclic_factor_cube(n)
+    v = np.array([N] + [0]*(2*n-2)).transpose()
+    rll = rl.copy()
+    rll[:,2*n-3] = v
+    print(n, end=': ')
+    print(script[2*n-2])
+    print(np.linalg.det(rll))
+    print(np.linalg.det(rl))
+    print(np.linalg.eigvals(rll))
+    print(np.linalg.eigvals(rl))
+    print(rll)
+    print(l)
+    print("--------------")  
+
+
+# for n,script in data.items():
+#     if n > 6:
+#         break
+#     g =  ncube_graph(n)
+#     l = g.laplacianMatrix()
+#     N = get_largest_cyclic_factor_cube(n)
+#     v = np.array([N] + [0]*(2**(n)-2)).transpose()
+#     rl = np.delete(np.delete(l, 1, 0), 1, 1)
+#     rll = rl.copy()
+#     rll[:,2**(n)-3] = v
+#     print(n, end=': ')
+#     print(script[2*n-2])
+#     print(np.linalg.det(rll))
+#     print(np.linalg.det(rl))
+#     print(np.linalg.det(rll)/np.linalg.det(rl))
+#     print(np.linalg.eigvals(rll))
+#     print("--------------")
+
+for n,script in data.items():
+    print(n)
+    for k in range(1,n-2):
+        print(k)
+        print(n * script[2*k])
+        print(script[2*k+1] + (n-1-k)*script[2*k+2] + (k) * script[2*k-2])
+        print("-----------")
+    print("===============")
+
+
+# def binomial(n, k):
+#     if k == 0:
+#         return 1
+#     return math.comb(n, k)
+
+# for n,script in data.items():
+#     print(n)
+#     for k in range(1,n-2):
+#         print(binomial(n-2,k-1))
+#         print(k/(n-k-1) * binomial(n-2,k))
+#         print("-----------")
+#     print("===============")
+        
